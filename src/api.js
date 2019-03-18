a20178c25fef96676bd4dffe10bc7dec0f4cc727
Sam McGrath <mcg13004@byui.edu>
2019-03-18 14:34:49 -0600

    [CORE] checkers completed. need to finish master checker

--- src/api.js
+++ src/api.js
@@ -39,31 +39,132 @@ async function checkGradebookActivity(courseId) {
 
 /**************************************************************
  * checkSubAccount
- * @param {Object} course
+ * @param {Object} firstCourse
+ * @param {Object} firstCourse
+ * @returns {Bool}
  * 
  * 
  *************************************************************/
-async function checkSubAccount(course) {
+async function checkSubAccount(firstCourse, secondCourse) {
+    let firstCourse = await canvas.get(`/api/v1/courses/${firstCourse.courseId}?include[]=sections&include[]=term&include[]=account`);
+    let secondCourse = await canvas.get(`/api/v1/courses/${secondCourse.courseId}?include[]=sections&include[]=term&include[]=account`);
 
+    if ((!firstCourse.account ||
+            !secondCourse.account) ||
+        (!firstCourse.account.id ||
+            !secondCourse.account.id)) return false;
+
+    return firstCourse.account.id === secondCourse.account.id;
 }
 
 /**************************************************************
  * checkTerm
- * @param {Object} course
+ * @param {Object} firstCourse
+ * @param {Object} firstCourse
+ * @returns {Bool}
  * 
  * 
  *************************************************************/
-async function checkTerm(course) {
-    let termMap = await helpers.mapTermToInteger();
+async function checkTerm(firstCourse, secondCourse) {
+    let firstCourse = await canvas.get(`/api/v1/courses/${firstCourse.courseId}?include[]=sections&include[]=term&include[]=account`);
+    let secondCourse = await canvas.get(`/api/v1/courses/${secondCourse.courseId}?include[]=sections&include[]=term&include[]=account`);
+
+    if ((!firstCourse.term ||
+            !secondCourse.term) ||
+        (!firstCourse.term.id ||
+            !secondCourse.term.id)) return false;
+
+    return firstCourse.term.id === secondCourse.term.id;
 }
 
 /**************************************************************
  * checkSections
- * @param {Object} course
+ * @param {Int} courseId
  * 
  * 
  *************************************************************/
-async function checkSections(course) {
+async function checkSections(courseId) {
+    let sectionsCourse = await canvas.get(`/api/v1/courses/${courseId}/sections`);
+
+    return sectionsCourse > 0;
+}
+
+async function retrieveCourses(coursesToRetrieve) {
+    let courseObjects = [];
+
+    for (let course in coursesToRetrieve) {
+        let courseObject = await canvas.get(`/api/v1/courses/${course.section}?include[]=sections&include[]=term&include[]=account`);
+        courseObjects.push(courseObject);
+    }
+
+    return courseObjects;
+}
+
+/**************************************************************
+ * masterChecker
+ * @param {Object} crossListObject
+ * @returns {Object}
+ * 
+ * This is the master for all checks and makes sure that all
+ * of the requirements are checked before initiating the API
+ * call to do the cross list. This will return an object
+ * like this: 
+ * 
+ * {
+ *      pass: true/false
+ *      reason: (this will be NULL or the requirement it failed)
+ * }
+ * 
+ * Whatever this function returns is what will be put into the 
+ * report.
+ *************************************************************/
+async function masterChecker(crossListObject) {
+    //check grade book activity
+    //same subaccount
+    //moving to a course that has no sections
+    //moving from a course that has more than one section
+
+    let checkers = [];
+    let destination = crossListObject.destination;
+    let sources = crossListObject.sources;
+
+    /******************** GRADE ACTIVITY *******************************/
+    let gradeActivityPass = true;
+    for (let source of sources) {
+        let gradeActivityResults = await checkGradebookActivity(source.courseId);
+        if (gradeActivityResults.events.length > 0) gradeActivityPass = false;
+    }
+
+    checkers.push(gradeActivityPass);
+
+    /******************* SAME SUBACCOUNT ******************************/
+    let sameSubAccountPass = true;
+    for (let source of sources) {
+        let subAccountResults = await checkSubAccount(destination, source);
+        if (!subAccountResults) sameSubAccountPass = false;
+    }
+
+    checkers.push(sameSubAccountPass);
+
+    /****************** SAME TERM ************************************/
+    let sameTermPass = true;
+    for (let source of sources) {
+        let sameTermResults = await checkTerm(destination, source);
+        if (!sameTermResults) sameTermPass = false;
+    }
+
+    checkers.push(sameTermPass);
+
+    /**************** SECTIONS (SOURCE) *****************************/
+    let sectionsPass = true;
+    for (let source of sources) {
+        let sectionsResults = await checkSections(source);
+        if (!sectionsResults) sectionsPass = false;
+    }
+
+    checkers.push(sectionsPass);
+
+    /**************** SECTIONS (DEST) ******************************/
 
 }
 
@@ -74,7 +175,9 @@ async function checkSections(course) {
  * 
  *************************************************************/
 async function analyzeCrossListData(crossListData) {
-
+    //run masterChecker
+    //run api
+    //store result
 }
 
 /**************************************************************
